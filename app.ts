c083d9e0d35946060151c308f8e620923c3a2039
Godswillben <amogugodswill99@gmail.com>
2023-02-01 22:33:04 +0100

    typescript basics Fundamentals

--- app.ts
+++ app.ts
@@ -1,16 +1,3 @@
-// unkown types
-let userInput: unknown;
-let userName: string;
+const userNames = "Maximillian"
 
-userInput = 5;
-userInput = "max";
-if (typeof userInput === "string"){
-    userName = userInput
-}
-
-function generateError(message: string, code: number): never{
-    throw {message: message, errorCode: code}
-    // while (true) {}
-}
-
-generateError("An error occurred!", 500)
+console.log(userNames);
